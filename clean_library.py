7dd279208fdd3b3d7f2be5ee9f3ec2fb56d91372
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2015-01-12 15:49:24 -0500

    much faster, dumber version
    Doesn't both trying to parse, just picks out the citekey lines and
    doesn't print ones that have been been seen before

--- clean_library.py
+++ clean_library.py
@@ -1,26 +1,32 @@
 """
-Clean up the mess that Mendeley leaves behind when it exports to BibTeX
+Clean up the mess that Mendeley leaves behind when it exports to BibTeX:
+
+* Delete entries with duplicate citekeys (keep the first one encountered)
+* Remove url and month fields (because apacite tries to include them and
+  it's not necessary/is ugly)
+
+Dave Kleinschmidt, 2015
 """
 
-import bibtexparser
-from bibtexparser.bparser import BibTexParser
-from bibtexparser.customization import homogeneize_latex_encoding
 import sys
+import re
 
-parser = BibTexParser()
-parser.customization = homogeneize_latex_encoding
+include_this_entry = True
+ids_seen = set()
 
-bib = bibtexparser.load(sys.stdin, parser=parser)
+start_entry_re = re.compile('@\w+{(?P<id>\w+),')
+exclude_line_re = re.compile('^(month|url)')
 
-## loop over entries, removing dupes
-entries_nodupes = []
-ids = set()
-for entry in bib.entries:
-    if entry['id'] not in ids:
-        ids.add(entry['id'])
-        entries_nodupes.append(entry)
-bib.entries = entries_nodupes
-
-## write output to stdout
-bib_string = bibtexparser.dumps(bib)
-sys.stdout.write(bib_string.encode('utf-8'))
+for line in sys.stdin:
+    m = start_entry_re.match(line)
+    if m:
+        ## start of new entry. check for dupe
+        if m.group('id') in ids_seen:
+            include_this_entry = False
+            sys.stderr.write('Duplicate ID: ' + m.group('id') + '\n')
+        else:
+            include_this_entry = True
+            ids_seen.add(m.group('id'))
+    if include_this_entry:
+        if not exclude_line_re.match(line):
+            sys.stdout.write(line)
