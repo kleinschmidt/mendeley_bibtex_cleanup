2227281a2c9e1ef4dd76b7c10693544113d667ac
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2015-01-12 17:38:39 -0500

    changed output filename to be like it was before

--- templates/clean-library-auto.sh
+++ templates/clean-library-auto.sh
@@ -2,6 +2,6 @@
 
 library_dir={{LIBRARYDIR}}
 cd ${library_dir}
-./clean_library.py < library.bib > library_clean.bib 2> /dev/null
+./clean_library.py < library.bib > library-clean.bib 2> /dev/null
 
 /usr/bin/logger library.bib modified and clean_library.py run
